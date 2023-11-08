4f645915ecca156c9bc7db72c32075fd6f2171c0
Yoon Jun Pil <galaxy160102@gmail.com>
2023-11-08 22:04:11 +0900

    🛠️feature : Errorcode modification

--- src/main/java/com/devjun/carmunity/global/exception/ErrorCode.java
+++ src/main/java/com/devjun/carmunity/global/exception/ErrorCode.java
@@ -10,7 +10,9 @@ public enum ErrorCode {
 
     DUPLICATED_USER_NAME(HttpStatus.CONFLICT, "user name is duplicated"),
     DIFF_PASSWORD_CHECK(HttpStatus.BAD_REQUEST, "password check is different with password"),
-    PAGE_NUMBER_OUT_OF_BOUNDS(HttpStatus.BAD_REQUEST, "page number is wrong");
+    PAGE_NUMBER_OUT_OF_BOUNDS(HttpStatus.BAD_REQUEST, "page number is wrong"),
+    USER_NAME_NOT_FOUND(HttpStatus.BAD_REQUEST,"user name is not found"),
+    PASSWORD_INCORRECT(HttpStatus.BAD_REQUEST,"password is not correct");
 
     private HttpStatus status;
     private String message;
